5b9c39a514f8831a8a548ee7280b94a43fad1170
Nelson Odins-Jones <yestacotaco@gmail.com>
2016-05-12 21:30:53 +1000

    respect the robot

--- mrPass.c
+++ mrPass.c
@@ -2,7 +2,10 @@
  *  Mr Pass.  Brain the size of a planet!
  *
  *  Proundly Created by Richard Buckland
- *  Share Freely Creative Commons SA-BY-NC 3.0. 
+ *  Share Freely Creative Commons SA-BY-NC 3.0.
+ *
+ *  FINISHED BY NELSON AND ED
+ *  RESPECT THE ROBOT
  *
  */
 
@@ -13,11 +16,28 @@
 #include "Game.h"
 #include "mechanicalTurk.h"
 
-action decideAction (Game g) {
+#define SPINOFF_COST { 0, 0, 0, 1, 1, 1 }
 
-   
+action decideAction (Game g) {
+   int player = 1;
+
+   int cost[NUM_DISCIPLINES] = SPINOFF_COST;
+
+   int numMJ = getStudents (g, player, STUDENT_MJ);
+   int numMTV = getStudents (g, player, STUDENT_MTV);
+   int numMMONEY = getStudents (g, player, STUDENT_MMONEY);
+
+
+   int canSpinoff = numMJ > cost[STUDENT_MJ] &&
+                    numMTV > cost[STUDENT_MTV] &&
+                    numMMONEY > cost[STUDENT_MMONEY];
+
    action nextAction;
-   nextAction.actionCode = PASS;
-   
+   if (canSpinoff) {
+      nextAction.actionCode = START_SPINOFF;
+   } else {
+      nextAction.actionCode = PASS;
+   }
+
    return nextAction;
 }
